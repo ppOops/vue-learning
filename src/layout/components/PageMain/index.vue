7ab4af10d301205546452cc5eef21a7fe557a6d5
ppOops <40376058+ppOops@users.noreply.github.com>
2022-09-09 00:04:13 +0800

    Add files via upload

--- src/layout/components/PageMain/index.vue
+++ src/layout/components/PageMain/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="pageMain">
     <transition name="fade-page" mode="out-in">
-      <keep-alive>
-        <router-view v-if="!$route.meta.noCache"></router-view>
-      </keep-alive>
-      <router-view v-if="$route.meta.noCache"></router-view>
+      <div v-if="$route.meta.noCache">
+        <keep-alive>
+          <router-view></router-view>
+        </keep-alive>
+      </div>
+      <router-view v-if="!$route.meta.noCache"></router-view>
     </transition>
   </div>
 </template>
-
 <script>
 export default {}
 </script>
